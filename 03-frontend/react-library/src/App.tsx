110aaec736303789708f81804e42252554727c22
Jerry Tsai <x22186565@student.ncirl.ie>
2024-01-14 20:36:20 +0000

    first commit

---
+++ 03-frontend/react-library/src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './App.css';
+import { Navbar } from './layouts/NavbarAndFooter/Navbar';
+
+function App() {
+  return (
+     <Navbar/>
+  );
+}
+
+export default App;
